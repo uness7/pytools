4af088ffc39f6aed26ff62be2ae220f2b5dcf07a
Youness Zioual <crness2@gmail.com>
2026-02-04 11:49:03 +0100

    mod: added some explanation of how we can use decorators differently

--- decorators/backbone.py
+++ decorators/backbone.py
@@ -11,3 +11,11 @@ def decorator_backbone(func):
 @decorator_backbone
 def some_other_function(params):
     pass
+
+# Another usage of the decorator exploiting the underlying concept of closures
+def some_other_function1(params):
+    pass
+
+some_other_function1_decorated = decorator_backbone(some_other_function1)
+some_other_function1_decorated()
+
